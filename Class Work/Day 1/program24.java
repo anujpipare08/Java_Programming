ee813413c3125ccdaa28de4d361290df7bec342e
Anuj <anujpipare8@gmail.com>
2025-10-14 14:00:16 +0530

    Java codes

---
+++ Class Work/Day 1/program24.java
@@ -0,0 +1,25 @@
+
+
+class Marvellous
+{
+    public  void Display()
+    {
+    int icnt = 0;
+    for(icnt=1;icnt<=4;icnt++)
+    {
+        System.out.println("Jay Ganesh...");
+    }
+    }
+
+}
+class program24
+{
+    public static void main(String A[])
+    {
+        Marvellous mobj = new Marvellous();
+
+        mobj.Display();
+        
+    }
+ }
+    
