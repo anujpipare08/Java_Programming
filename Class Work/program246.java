26119079adc35fe92f039498d2f1ed1f32dfabac
Anuj <anujpipare8@gmail.com>
2026-02-12 21:12:43 +0530

    .

---
+++ Class Work/program246.java
@@ -0,0 +1,21 @@
+// Hello
+// H    e   l   l   0
+import java.util.*;
+import javax.tools.SimpleJavaFileObject;
+class program246
+{
+    public static void main(String A[])
+    {
+       Scanner scanobj = new Scanner(System.in);
+
+        System.out.println("Enter string :");
+        String sobj = scanobj.nextLine();
+
+        char Arr[] = sobj.toCharArray();
+        System.err.println(Arr[0]);
+         System.err.println(Arr[1]);
+          System.err.println(Arr[2]);
+           System.err.println(Arr[3]);
+            System.err.println(Arr[4]);
+    }
+}
