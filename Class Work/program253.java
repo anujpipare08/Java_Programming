10793ba5fdcfdb0f5d96a442d2f94160ffc0ee89
Anuj <anujpipare8@gmail.com>
2026-02-15 11:16:54 +0530

    .

---
+++ Class Work/program253.java
@@ -0,0 +1,32 @@
+// Hello
+// _    e   l   l   o
+import java.util.*;
+
+class StringX
+{
+    public String Update(String str)
+    {
+       char Arr[] = str.toCharArray();
+
+       Arr[0] = '_';
+       return new String(Arr); //change
+    }
+}
+class program253
+{
+    public static void main(String A[])
+    {
+        String sRet = null;
+        Scanner scanobj = new Scanner(System.in);
+
+        System.out.println("Enter string :");
+        String sobj = scanobj.nextLine();    
+
+        StringX strobj = new StringX();
+
+        sRet = strobj.Update(sobj);
+
+        System.err.println("updated string :"+sRet);
+
+    }
+}
